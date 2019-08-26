3ebf5400929e647cb8e4c82918a4e471edf17cbc
Eddie Fann <efann@beowurks.com>
2019-08-26 16:24:59 -0500

    Added field for 1099 Taxable

---
+++ src/com/beowurks/jequity/dao/migration/V3_7__Add_Boolean_Field.java
@@ -0,0 +1,31 @@
+/*
+ * JEquity
+ * Copyright(c) 2008-2019, Beowurks
+ * Original Author: Eddie Fann
+ * License: Eclipse Public License - v 2.0 (https://www.eclipse.org/org/documents/epl-2.0/EPL-2.0.html)
+ *
+ */
+package com.beowurks.jequity.dao.migration;
+
+import org.flywaydb.core.api.migration.Context;
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+public class V3_7__Add_Boolean_Field extends MigrationHelper
+{
+
+  // -----------------------------------------------------------------------------
+  @Override
+  public void migrate(final Context toContext) throws Exception
+  {
+    this.foConnection = toContext.getConnection();
+
+    this.executeStatements("AddBooleanField.sql");
+  }
+  // -----------------------------------------------------------------------------
+
+}
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
