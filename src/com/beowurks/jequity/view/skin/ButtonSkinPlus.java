e81c87e2157bd7a60ab177e2c432ad82ab7eabca
Eddie Fann <efann@beowurks.com>
2022-06-29 21:45:56 -0500

    Buttons on hover now fade

---
+++ src/com/beowurks/jequity/view/skin/ButtonSkinPlus.java
@@ -0,0 +1,46 @@
+/*
+ * JEquity
+ * Copyright(c) 2008-2022, Beowurks
+ * Original Author: Eddie Fann
+ * License: Eclipse Public License - v 2.0 (https://www.eclipse.org/org/documents/epl-2.0/EPL-2.0.html)
+ *
+ */
+
+package com.beowurks.jequity.view.skin;
+
+import javafx.animation.FadeTransition;
+import javafx.scene.control.Button;
+import javafx.scene.control.skin.ButtonSkin;
+import javafx.util.Duration;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
+// From https://stackoverflow.com/questions/49831755/javafx-animation-on-button-hover
+// By the way, it's used in com/beowurks/jequity/view/css/Main.css
+public class ButtonSkinPlus extends ButtonSkin
+{
+
+  // ---------------------------------------------------------------------------------------------------------------------
+  public ButtonSkinPlus(final Button toControl)
+  {
+    super(toControl);
+
+    final FadeTransition loFadeIn = new FadeTransition(Duration.millis(200));
+    loFadeIn.setNode(toControl);
+    loFadeIn.setToValue(1);
+
+    final FadeTransition loFadeOut = new FadeTransition(Duration.millis(200));
+    loFadeOut.setNode(toControl);
+    loFadeOut.setToValue(0.75);
+
+    toControl.setOnMouseEntered(e -> loFadeOut.playFromStart());
+    toControl.setOnMouseExited(e -> loFadeIn.playFromStart());
+  }
+  // ---------------------------------------------------------------------------------------------------------------------
+
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
