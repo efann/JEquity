a22cc7171ab03a043b0acd82fc66bcb312baaa9b
Eddie Fann <efann@beowurks.com>
2025-12-15 10:56:56 -0600

    Tweaking the bash script to generate the code-signing key.

--- certificate/generate.sh
+++ certificate/generate.sh
@@ -7,22 +7,35 @@
 #
 #
 
-if [ -z "$1" ]; then
-  echo -e "\nOne must pass the password for the private key. Exiting. . . .\n\n"
+if [[ -z "$1" || -z "$2" ]]; then
+  echo -e "\n"
+  echo -e "You must run this script in a Linux environment.\n"
+  echo -e "1st parameter: password for the private key."
+  echo -e "2nd parameter: password for the PKCS12 keystore."
+  echo -e "\nExiting. . . .\n\n"
   exit 1
 fi
 
-if [ -z "$2" ]; then
-  echo -e "\nOne must pass the password for the PKCS12 keystore. Exiting. . . .\n\n"
-  exit 1
-fi
+LINE_MARKER="\n========================================================================================"
+BASE_FILE='rootSSO'
+
+echo -e $LINE_MARKER
+echo -e "\nRemoving previous $BASE_FILE files."
+rm -v $BASE_FILE.*
+echo -e $LINE_MARKER
 
 echo -e "This script generates a self-signed x509 certificate on Linux that's valid for 10 years.\nRunning the following:\n"
-echo -e "openssl genrsa -aes256 -out rootSSO.key 16384"
+echo -e "\nopenssl genrsa -aes256 -passout pass:$1 -out $BASE_FILE.key 16384"
+openssl genrsa -aes256 -passout pass:"$1" -out $BASE_FILE.key 16384
 
-openssl genrsa -aes256 -out rootSSO.key 16384 -passout pass:"$1"
+if [ $? -ne 0 ]; then
+  echo "An error occurred with 'openssl genrsa -aes256" >&2
+  exit 1
+fi
+echo -e $LINE_MARKER
 
-openssl req -x509 -new -nodes -key rootSSO.key -sha512 -days 3650 -passin pass:"$1" -out rootSSO.pem<<EOF
+echo -e "\nopenssl req -x509 -new -nodes -key $BASE_FILE.key -sha512 -days 3650 -passin pass:$1 -out $BASE_FILE.pem"
+openssl req -x509 -new -nodes -key $BASE_FILE.key -sha512 -days 3650 -passin pass:"$1" -out $BASE_FILE.pem<<EOF
 US
 Texas
 Austin
@@ -32,7 +45,20 @@ Open-Source Software Root Certificate
 efann@beowurks.com
 EOF
 
-openssl x509 -text -noout -in rootSSO.pem
-# Combine them into a PKCS12 keystore (my-keystore.pfx)
-openssl pkcs12 -export -out rootSSO.pfx -inkey rootSSO.key -in rootSSO.pem -name "SSO Certificate" -passin pass:"$1" -passout pass:"$2"
+if [ $? -ne 0 ]; then
+  echo "An error occurred with 'openssl req -x509'" >&2
+  exit 1
+fi
+echo -e $LINE_MARKER
 
+openssl x509 -text -noout -in $BASE_FILE.pem
+# Combine them into a PKCS12 keystore (my-keystore.pfx)
+echo -e "\nopenssl pkcs12 -export -out $BASE_FILE.pfx -inkey $BASE_FILE.key -in $BASE_FILE.pem -name SSO Certificate -passin pass:$1 -passout pass:$2"
+openssl pkcs12 -export -out $BASE_FILE.pfx -inkey $BASE_FILE.key -in $BASE_FILE.pem -name "SSO Certificate" -passin pass:"$1" -passout pass:"$2"
+if [ $? -ne 0 ]; then
+  echo "An error occurred with 'openssl pkcs12 -export'" >&2
+  exit 1
+fi
+echo -e $LINE_MARKER
+
+echo -e "\n\nNow copy $BASE_FILE.* over to the folder of your choosing."
